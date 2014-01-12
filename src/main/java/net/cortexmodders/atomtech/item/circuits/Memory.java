ec6315b48a010968655529ba5e2667c80a408f13
Matthew Warren <tonkamatt98@gmail.com>
2014-01-12 13:49:34 -0600

    updated circuits

---
+++ src/main/java/net/cortexmodders/atomtech/item/circuits/Memory.java
@@ -0,0 +1,49 @@
+package net.cortexmodders.atomtech.item.circuits;
+
+import net.cortexmodders.atomtech.item.circuits.components.Transistor;
+import net.cortexmodders.atomtech.item.circuits.components.Wire;
+
+public class Memory
+{
+	private long bits;
+	
+	public Memory(Wire wire, Memory[] memoryModules)
+	{
+		if(wire != null)
+		{
+			for(Memory memory : memoryModules)
+			{
+				bits += memory.bits;
+			}
+		}
+		else
+			throw new IllegalArgumentException("Wire cannot be null");
+	}
+	
+	public Memory(Wire wire, Transistor[] transistors)
+	{
+		if(wire != null)
+		{
+			for(Transistor t : transistors)
+			{
+				if(t != null)
+					bits++;
+			}
+		}
+		else
+			throw new IllegalArgumentException("Wire cannot be null");
+	}
+	
+	public Memory(Transistor t)
+	{
+		if(t != null)
+			bits = 1;
+		else
+			throw new IllegalArgumentException("Transistor cannot be null");
+	}
+	
+	public long size()
+	{
+		return bits;
+	}
+}
--- src/main/scala/net/cortexmodders/atomtech/item/circuits/components/ElectricalComponent.scala
+++ src/main/scala/net/cortexmodders/atomtech/item/circuits/components/ElectricalComponent.scala
@@ -7,21 +7,56 @@ class ElectricalComponentWrapper(id: Integer) extends Item(id)
 	
 }
 
+class ElectricalUnit(val amperes: Double, val volts: Double)
+{
+	object Utilities {
+		def getResistance(start: ElectricalUnit, end: ElectricalUnit) = {
+			start.volts / start.amperes - end.volts / end.amperes
+		}
+	}
+}
+
 abstract class ElectricalComponent(val maxAmperes: Double, val maxVolts: Double, val providedResistance: Double)
 {
 	protected var fried = false
 	// TODO name it something better
-	def process(amps: Double, volts: Double)
+	def process(electricity: ElectricalUnit): ElectricalUnit
+	def shouldFry(electricity: ElectricalUnit) = {
+		if(electricity.volts > maxVolts || electricity.amperes > maxAmperes)
+			true
+		false
+	}
 	def fry = fried = true
 	def isFried = fried
 	def maxWatts = maxAmperes * maxVolts
+}
+
+class Wire(maxAmps: Double, maxVolts: Double) extends ElectricalComponent(maxAmps, maxVolts, 0.0D)
+{
+	def process(electricity: ElectricalUnit) = {
+		if(shouldFry(electricity))
+		{
+			fry
+			null
+		}
+		electricity
+	}
 }
 
 class Transistor(val minVolts: Double, maxAmps: Double, maxVolts: Double) extends ElectricalComponent(maxAmps, maxVolts, 0.0D)
 {
 	var on = false
 	// TODO
-	def process(amps: Double, volts: Double) = {
-		
+	def process(electricity: ElectricalUnit) = {
+		if(shouldFry(electricity))
+		{
+			fry
+			null
+		}
+		if(electricity.volts > minVolts)
+			on = true
+		else
+			on = false
+		electricity
 	}
 }
