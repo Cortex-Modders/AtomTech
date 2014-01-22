329a6e68af01866bbd29d5b7b9b2cc9bd2ee234f
Jadar <jake@jadarstudios.com>
2014-01-21 21:31:03 -0600

    added renderinghandler for inventory rendering.

--- src/main/java/net/cortexmodders/atomtech/client/render/tileentity/RenderSmallBattery.java
+++ src/main/java/net/cortexmodders/atomtech/client/render/tileentity/RenderSmallBattery.java
@@ -1,14 +1,18 @@
 package net.cortexmodders.atomtech.client.render.tileentity;
 
 import net.cortexmodders.atomtech.client.model.tileentity.ModelSmallBattery;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.IBlockAccess;
 
 import org.lwjgl.opengl.GL11;
 
+import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 
-public class RenderSmallBattery extends TileEntitySpecialRenderer
+public class RenderSmallBattery extends TileEntitySpecialRenderer implements ISimpleBlockRenderingHandler
 {
     
     ModelSmallBattery model;
@@ -36,6 +40,31 @@ public class RenderSmallBattery extends TileEntitySpecialRenderer
             model.render(0.0625F);
         }
         GL11.glPopMatrix();
+    }
+
+    @Override
+    public void renderInventoryBlock(Block block, int metadata, int modelID, RenderBlocks renderer)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean shouldRender3DInInventory()
+    {
+        return true;
+    }
+
+    @Override
+    public int getRenderId()
+    {
+        return 0;
     }
     
 }
